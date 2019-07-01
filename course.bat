4c5576b6e194440ae5101eff8d13e0af94333de0
Evgeniy <e.pogrebnyak@gmail.com>
2019-07-01 19:31:26 +0300

    упрощенное введение

--- course.bat
+++ course.bat
@@ -1,9 +1,6 @@
 @ECHO OFF
-REM SETLOCAL
-REM SET URL=https://gist.githubusercontent.com/epogrebnyak/285346a623bcf670f05ffaaa7f16162b/raw/bd205b8407741d412c6f3aee072edb6d859e954d/architecture_notes.txt
-SET FILENAME=nes-bank-credit-course
-
-REM rem Download URL as FILENAME.docx
-REM curl %URL% | pandoc -f markdown -t docx -o %FILENAME%.docx
-REM ENDLOCAL
+SETLOCAL
+SET FILENAME=README
 pandoc -f markdown -t docx README.md -o %FILENAME%.docx
+curl https://gitprint.com/epogrebnyak/notes-credit-risk/blob/master/README.md > %FILENAME%.pdf
+ENDLOCAL
