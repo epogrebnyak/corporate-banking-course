5fb18f8df9718af3f810b7a5ccd76fcab2c8984d
Evgeniy <e.pogrebnyak@gmail.com>
2019-07-02 09:59:55 +0300

    edits in text

--- course.bat
+++ course.bat
@@ -2,5 +2,4 @@
 SETLOCAL
 SET FILENAME=README
 pandoc -f markdown -t docx README.md -o %FILENAME%.docx
-curl https://gitprint.com/epogrebnyak/notes-credit-risk/blob/master/README.md > %FILENAME%.pdf
 ENDLOCAL
